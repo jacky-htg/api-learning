fb4b9f1805eb764ac70d10a1f4b720d33c2c9cba
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-05-24 16:44:20 +0700

    02-shutdown

---
+++ 02-shutdown/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	server := http.Server{
+		Addr:         "localhost:9000",
+		Handler:      http.HandlerFunc(helloworld),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Println("server listening on", server.Addr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("error: listening and serving: %s", err)
+
+	case <-osSignals:
+		log.Println("caught signal, shutting down")
+
+		// Give outstanding requests 5 seconds to complete.
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("error: gracefully shutting down server: %s", err)
+			if err := server.Close(); err != nil {
+				log.Printf("error: closing server: %s", err)
+			}
+		}
+	}
+
+	log.Println("done")
+}
+
+// helloworld: basic http handler with response hello world string
+func helloworld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+}
