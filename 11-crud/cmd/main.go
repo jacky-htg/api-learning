8785d6c5593d40ba592463752c07ee05dec291a9
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-07-31 17:21:24 +0700

    add crud module

---
+++ 11-crud/cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jacky-htg/go-services/schema"
+	"github.com/jacky-htg/go-services/services/config"
+	"github.com/jacky-htg/go-services/services/database"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	_, ok := os.LookupEnv("APP_ENV")
+	if !ok {
+		config.Setup(".env")
+	}
+
+	if err := run(); err != nil {
+		log.Printf("error: shutting down: %s", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+
+	flag.Parse()
+
+	// =========================================================================
+	// Start Database
+
+	db, err := database.Open()
+	if err != nil {
+		return errors.Wrap(err, "connecting to db")
+	}
+	defer db.Close()
+
+	switch flag.Arg(0) {
+	case "migrate":
+		if err := schema.Migrate(db); err != nil {
+			return errors.Wrap(err, "applying migrations")
+		}
+		fmt.Println("Migrations complete")
+
+	case "seed":
+		if err := schema.Seed(db); err != nil {
+			return errors.Wrap(err, "seeding database")
+		}
+		fmt.Println("Seed data complete")
+	}
+
+	return nil
+}
---
+++ 11-crud/packages/auth/controllers/users.go
@@ -0,0 +1,262 @@
+package controllers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
+	"github.com/jacky-htg/go-services/packages/auth/controllers/request"
+	"github.com/jacky-htg/go-services/packages/auth/controllers/response"
+	"github.com/jacky-htg/go-services/packages/auth/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//Users : struct for set Users Dependency Injection
+type Users struct {
+	Db  *sql.DB
+	Log *log.Logger
+}
+
+//List : http handler for returning list of users
+func (u *Users) List(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	list, err := user.List(u.Db)
+	if err != nil {
+		u.Log.Printf("error call list users: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var userResponse response.UserResponse
+	var listResponse []*response.UserResponse
+	for _, user := range list {
+		listResponse = append(listResponse, userResponse.Transform(&user))
+	}
+
+	data, err := json.Marshal(listResponse)
+	if err != nil {
+		u.Log.Println("error marshalling result", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		u.Log.Println("error writing result", err)
+	}
+}
+
+//View : http handler for retrieve user by id
+func (u *Users) View(w http.ResponseWriter, r *http.Request) {
+	paramID := chi.URLParam(r, "id")
+
+	id, err := strconv.Atoi(paramID)
+	if err != nil {
+		u.Log.Printf("error type casting paramID: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var user models.User
+	user, err = user.Get(u.Db, int64(id))
+	if err != nil {
+		u.Log.Printf("error call list user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var response response.UserResponse
+	data, err := json.Marshal(response.Transform(&user))
+	if err != nil {
+		u.Log.Println("error marshalling result", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		u.Log.Println("error writing result", err)
+	}
+}
+
+//Create : http handler for create new user
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var userRequest request.NewUserRequest
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&userRequest)
+	if err != nil {
+		u.Log.Printf("error decode user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if userRequest.Password != userRequest.RePassword {
+		err = errors.New("Password not match")
+		u.Log.Printf("error : %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	pass, err := bcrypt.GenerateFromPassword([]byte(userRequest.Password), bcrypt.DefaultCost)
+	if err != nil {
+		u.Log.Printf("error generate password: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	userRequest.Password = string(pass)
+
+	user := userRequest.Transform()
+
+	user, err = user.Create(u.Db)
+	if err != nil {
+		u.Log.Printf("error call create user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var response response.UserResponse
+	data, err := json.Marshal(response.Transform(user))
+	if err != nil {
+		u.Log.Println("error marshalling result", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		u.Log.Println("error writing result", err)
+	}
+}
+
+//Update : http handler for update user by id
+func (u *Users) Update(w http.ResponseWriter, r *http.Request) {
+	paramID := chi.URLParam(r, "id")
+
+	id, err := strconv.Atoi(paramID)
+	if err != nil {
+		u.Log.Printf("error type casting paramID: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var user models.User
+	user, err = user.Get(u.Db, int64(id))
+	if err != nil {
+		u.Log.Printf("error call list user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if user.ID <= 0 {
+		err = errors.New("User not found")
+		u.Log.Printf("error user not found: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var userRequest request.UserRequest
+
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&userRequest)
+	if err != nil {
+		u.Log.Printf("error decode user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if len(userRequest.Password) > 0 && userRequest.Password != userRequest.RePassword {
+		err = errors.New("Password not match")
+		u.Log.Printf("error : %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if len(userRequest.Password) > 0 {
+		pass, err := bcrypt.GenerateFromPassword([]byte(userRequest.Password), bcrypt.DefaultCost)
+		if err != nil {
+			u.Log.Printf("error generate password: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		userRequest.Password = string(pass)
+	}
+
+	if userRequest.ID <= 0 {
+		userRequest.ID = user.ID
+	}
+	userUpdate := userRequest.Transform(&user)
+
+	userUpdate, err = userUpdate.Update(u.Db)
+	if err != nil {
+		u.Log.Printf("error call update user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var response response.UserResponse
+	data, err := json.Marshal(response.Transform(userUpdate))
+	if err != nil {
+		u.Log.Println("error marshalling result", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		u.Log.Println("error writing result", err)
+	}
+}
+
+//Delete : http handler for delete user by id
+func (u *Users) Delete(w http.ResponseWriter, r *http.Request) {
+	paramID := chi.URLParam(r, "id")
+
+	id, err := strconv.Atoi(paramID)
+	if err != nil {
+		u.Log.Printf("error type casting paramID: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var user models.User
+	user, err = user.Get(u.Db, int64(id))
+	if err != nil {
+		u.Log.Printf("error call list user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if user.ID <= 0 {
+		err = errors.New("User not found")
+		u.Log.Printf("error user not found: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	isDelete, err := user.Delete(u.Db)
+	if err != nil {
+		u.Log.Printf("error call delete user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if !isDelete {
+		err = errors.New("error delete user")
+		u.Log.Printf("error call delete user: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
