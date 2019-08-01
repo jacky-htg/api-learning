13a67cd302ed83ce2c2ae3f1f0f5aa1a5592d125
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-08-01 13:12:29 +0700

    change schema using sqlx

--- 06-configuration/cmd/main.go
+++ 06-configuration/cmd/main.go
@@ -23,14 +23,19 @@ func main() {
 	// =========================================================================
 	// Start Database
 
-	db, err := database.Open()
+	dbx, err := database.Openx()
 	if err != nil {
 		log.Fatalf("error: connecting to db: %s", err)
 	}
-	defer db.Close()
+	defer dbx.Close()
 
 	switch flag.Arg(0) {
 	case "migrate":
+		db, err := database.Open()
+		if err != nil {
+			log.Fatalf("error: connecting to db: %s", err)
+		}
+		defer db.Close()
 		if err := schema.Migrate(db); err != nil {
 			log.Println("error applying migrations", err)
 			os.Exit(1)
@@ -39,7 +44,7 @@ func main() {
 		return
 
 	case "seed":
-		if err := schema.Seed(db); err != nil {
+		if err := schema.Seed(dbx); err != nil {
 			log.Println("error seeding database", err)
 			os.Exit(1)
 		}
