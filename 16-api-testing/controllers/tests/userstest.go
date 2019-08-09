46617fe4bc29125d28a701af6ae207759d7a5b2f
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-08-09 14:22:13 +0700

    change api test to crud

--- 16-api-testing/controllers/tests/userstest.go
+++ 16-api-testing/controllers/tests/userstest.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -44,37 +45,16 @@ func (u *Users) List(t *testing.T) {
 	}
 }
 
-//View : http handler for retrieve user by id
-func (u *Users) View(t *testing.T) {
-	req := httptest.NewRequest("GET", "/users/1", nil)
-	req.Header.Set("Content-Type", "application/json")
-	resp := httptest.NewRecorder()
-
-	u.App.ServeHTTP(resp, req)
-
-	if http.StatusOK != resp.Code {
-		t.Fatalf("retrieving: expected status code %v, got %v", http.StatusOK, resp.Code)
-	}
-
-	var fetched map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&fetched); err != nil {
-		t.Fatalf("decoding: %s", err)
-	}
-
-	want := map[string]interface{}{
-		"id":        float64(1),
-		"username":  "jackyhtg",
-		"is_active": true,
-	}
-
-	// Fetched product should match the one we created.
-	if diff := cmp.Diff(want, fetched); diff != "" {
-		t.Fatalf("Retrieved user should match created. Diff:\n%s", diff)
-	}
+//Crud : http handler for users crud
+func (u *Users) Crud(t *testing.T) {
+	created := u.Create(t)
+	u.View(t, created["id"].(float64))
+	u.Update(t, created["id"].(float64))
+	u.Delete(t, created["id"].(float64))
 }
 
 //Create : http handler for create new user
-func (u *Users) Create(t *testing.T) {
+func (u *Users) Create(t *testing.T) map[string]interface{} {
 	var created map[string]interface{}
 	jsonBody := `
 		{
@@ -114,14 +94,45 @@ func (u *Users) Create(t *testing.T) {
 	if diff := cmp.Diff(want, created); diff != "" {
 		t.Fatalf("Response did not match expected. Diff:\n%s", diff)
 	}
+
+	return created
+}
+
+//View : http handler for retrieve user by id
+func (u *Users) View(t *testing.T, id float64) {
+	req := httptest.NewRequest("GET", "/users/"+fmt.Sprintf("%d", int(id)), nil)
+	req.Header.Set("Content-Type", "application/json")
+	resp := httptest.NewRecorder()
+
+	u.App.ServeHTTP(resp, req)
+
+	if http.StatusOK != resp.Code {
+		t.Fatalf("retrieving: expected status code %v, got %v", http.StatusOK, resp.Code)
+	}
+
+	var fetched map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&fetched); err != nil {
+		t.Fatalf("decoding: %s", err)
+	}
+
+	want := map[string]interface{}{
+		"id":        id,
+		"username":  "peterpan",
+		"is_active": true,
+	}
+
+	// Fetched product should match the one we created.
+	if diff := cmp.Diff(want, fetched); diff != "" {
+		t.Fatalf("Retrieved user should match created. Diff:\n%s", diff)
+	}
 }
 
 //Update : http handler for update user by id
-func (u *Users) Update(t *testing.T) {
+func (u *Users) Update(t *testing.T, id float64) {
 	var updated map[string]interface{}
 	jsonBody := `
 		{
-			"id": 1,
+			"id": %s,
 			"username": "gatholoco",
 			"email": "gatholoco@gmail.com", 
 			"password": "1234", 
@@ -129,9 +140,9 @@ func (u *Users) Update(t *testing.T) {
 			"is_active": false
 		}
 	`
-	body := strings.NewReader(jsonBody)
+	body := strings.NewReader(fmt.Sprintf(jsonBody, fmt.Sprintf("%d", int(id))))
 
-	req := httptest.NewRequest("PUT", "/users/1", body)
+	req := httptest.NewRequest("PUT", "/users/"+fmt.Sprintf("%d", int(id)), body)
 	req.Header.Set("Content-Type", "application/json")
 	resp := httptest.NewRecorder()
 
@@ -146,7 +157,7 @@ func (u *Users) Update(t *testing.T) {
 	}
 
 	want := map[string]interface{}{
-		"id":        float64(1),
+		"id":        id,
 		"username":  "gatholoco",
 		"is_active": false,
 	}
@@ -157,8 +168,8 @@ func (u *Users) Update(t *testing.T) {
 }
 
 //Delete : http handler for delete user by id
-func (u *Users) Delete(t *testing.T) {
-	req := httptest.NewRequest("DELETE", "/users/1", nil)
+func (u *Users) Delete(t *testing.T, id float64) {
+	req := httptest.NewRequest("DELETE", "/users/"+fmt.Sprintf("%d", int(id)), nil)
 	req.Header.Set("Content-Type", "application/json")
 	resp := httptest.NewRecorder()
 
--- 16-api-testing/main_test.go
+++ 16-api-testing/main_test.go
@@ -6,7 +6,8 @@ import (
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
-	userTest "github.com/jacky-htg/go-services/controllers/tests"
+	userApiTest "github.com/jacky-htg/go-services/controllers/tests"
+	userUnitTest "github.com/jacky-htg/go-services/models/tests"
 	"github.com/jacky-htg/go-services/routing"
 	"github.com/jacky-htg/go-services/schema"
 	"github.com/jacky-htg/go-services/services/config"
@@ -28,11 +29,14 @@ func TestMain(t *testing.T) {
 
 	log := log.New(os.Stderr, "TEST : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
-	users := userTest.Users{App: routing.API(db, log)}
+	//unit test for user
+	user := userUnitTest.User{Db: db}
+	t.Run("UnitUsersList", user.List)
+	t.Run("UnitUsersCrud", user.Crud)
 
-	t.Run("UsersList", users.List)
-	t.Run("UsersView", users.View)
-	t.Run("UsersCreate", users.Create)
-	t.Run("UsersUpdate", users.Update)
-	t.Run("UsersDelete", users.Delete)
+	// api test for users
+	users := userApiTest.Users{App: routing.API(db, log)}
+
+	t.Run("APiUsersList", users.List)
+	t.Run("APiUsersCrud", users.Crud)
 }
