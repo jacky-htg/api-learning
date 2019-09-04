70d99bff88158b0d0a7525ab10c1aa3d68e8ae7c
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-09-04 16:43:22 +0700

    init rbac

---
+++ 22-authorization-using-rbac/controllers/auths.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jacky-htg/go-services/libraries/api"
+	"github.com/jacky-htg/go-services/libraries/token"
+	"github.com/jacky-htg/go-services/models"
+	"github.com/jacky-htg/go-services/payloads/request"
+	"github.com/jacky-htg/go-services/payloads/response"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//Auths : struct for set Auths Dependency Injection
+type Auths struct {
+	Db  *sqlx.DB
+	Log *log.Logger
+}
+
+//Login : http handler for login
+func (u *Auths) Login(w http.ResponseWriter, r *http.Request) error {
+	var loginRequest request.LoginRequest
+	err := api.Decode(r, &loginRequest)
+	if err != nil {
+		u.Log.Printf("ERROR : %+v", err)
+		return errors.Wrap(err, "decode auth")
+	}
+
+	uLogin := models.User{Username: loginRequest.Username}
+	err = uLogin.GetByUsername(r.Context(), u.Db)
+	if err != nil {
+		u.Log.Printf("ERROR : %+v", err)
+		return errors.Wrap(err, "call login")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(uLogin.Password), []byte(loginRequest.Password))
+	if err != nil {
+		u.Log.Printf("ERROR : %+v", err)
+		return errors.Wrap(err, "compare password")
+	}
+
+	token, err := token.ClaimToken(uLogin.Username)
+	if err != nil {
+		u.Log.Printf("ERROR : %+v", err)
+		return errors.Wrap(err, "claim token")
+	}
+
+	var response response.TokenResponse
+	response.Token = token
+
+	return api.ResponseOK(w, response, http.StatusOK)
+}
---
+++ 22-authorization-using-rbac/routing/route.go
@@ -0,0 +1,30 @@
+package routing
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jacky-htg/go-services/controllers"
+	"github.com/jacky-htg/go-services/libraries/api"
+	"github.com/jacky-htg/go-services/middleware"
+	"github.com/jmoiron/sqlx"
+)
+
+//API : handler api
+func API(db *sqlx.DB, log *log.Logger) http.Handler {
+	app := api.NewApp(log, middleware.Errors(log))
+
+	// Auth Routing
+	auth := controllers.Auths{Db: db, Log: log}
+	app.Handle(http.MethodPost, "/login", auth.Login)
+
+	// Users Routing
+	u := controllers.Users{Db: db, Log: log}
+	app.Handle(http.MethodGet, "/users", u.List)
+	app.Handle(http.MethodGet, "/users/{id}", u.View)
+	app.Handle(http.MethodPost, "/users", u.Create)
+	app.Handle(http.MethodPut, "/users/{id}", u.Update)
+	app.Handle(http.MethodDelete, "/users/{id}", u.Delete)
+
+	return app
+}
