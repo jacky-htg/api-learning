14247f9eb6bc8dc3a5af404bfe79e55f2860b083
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-09-11 16:41:02 +0700

    add health check feature

--- 23-health/README.md
+++ 23-health/README.md
@@ -1,41 +1,17 @@
 # Rebel Services
 
-Authorization using RBAC (role based access controller)  
+Health Check with objective to create  new /health endpoint that returns 200 when the database is ready.
 
 Tasks:
-- design rbac database on schema/migrate.go and schema/seed.go
-- run go cmd/main.go migrate && go cmd/main.go seed to dump database
-- design rbac routing on routing/route.go
-- create scan access command libraries/auth/access.go
-- run go cmd/main.go scan-access to insert routing into access table
-- create roles service
-- update users service to support roles/multi-roles
-- create middleware to handle authorization checking 
-- update api testing to support all feature in this chapter
+- Create StatusCheck function on libraries/database/database.go
+- Add a /health endpoint on routing/route.go
+- Create check controller on controllers/checks.go
 
 ## File Changes :
 - routing/route.go
-- models/user.go
-- controllers/users.go
-- schema/migrate.go
-- schema/seed.go
-- cmd/main.go
-- main_test.go
-- controllers/tests/userstest.go
+- libraries/database/database.go
 
 ## New File :
-- libraries/auth/access.go
-- controllers/access.go
-- models/access.go
-- payloads/response/access_response.go
-- controllers/roles.go
-- models/roles.go
-- payloads/request/roles_request.go
-- payloads/response/roles_response.go
-- middleware/auth.go
-- controllers/tests/authstest.go
+- controllers/checks.go
 
 ## Adding Dependency :
-
-## NOTE
-In this chapter, we already implementation of handling transaction
--- 23-health/routing/route.go
+++ 23-health/routing/route.go
@@ -22,31 +22,45 @@ func mid(db *sqlx.DB, log *log.Logger) []api.Middleware {
 func API(db *sqlx.DB, log *log.Logger) http.Handler {
 	app := api.NewApp(db, log, mid(db, log)...)
 
+	// Health Routing
+	{
+		check := Check{db: db}
+		app.Handle(http.MethodGet, "/health", check.Health)
+	}
+
 	// Auth Routing
-	auth := controllers.Auths{Db: db, Log: log}
-	app.Handle(http.MethodPost, "/login", auth.Login)
+	{
+		auth := controllers.Auths{Db: db, Log: log}
+		app.Handle(http.MethodPost, "/login", auth.Login)
+	}
 
 	// Users Routing
-	users := controllers.Users{Db: db, Log: log}
-	app.Handle(http.MethodGet, "/users", users.List)
-	app.Handle(http.MethodGet, "/users/{id}", users.View)
-	app.Handle(http.MethodPost, "/users", users.Create)
-	app.Handle(http.MethodPut, "/users/{id}", users.Update)
-	app.Handle(http.MethodDelete, "/users/{id}", users.Delete)
+	{
+		users := controllers.Users{Db: db, Log: log}
+		app.Handle(http.MethodGet, "/users", users.List)
+		app.Handle(http.MethodGet, "/users/{id}", users.View)
+		app.Handle(http.MethodPost, "/users", users.Create)
+		app.Handle(http.MethodPut, "/users/{id}", users.Update)
+		app.Handle(http.MethodDelete, "/users/{id}", users.Delete)
+	}
 
 	// Roles Routing
-	roles := controllers.Roles{Db: db, Log: log}
-	app.Handle(http.MethodGet, "/roles", roles.List)
-	app.Handle(http.MethodGet, "/roles/{id}", roles.View)
-	app.Handle(http.MethodPost, "/roles", roles.Create)
-	app.Handle(http.MethodPut, "/roles/{id}", roles.Update)
-	app.Handle(http.MethodDelete, "/roles/{id}", roles.Delete)
-	app.Handle(http.MethodPost, "/roles/{id}/access/{access_id}", roles.Grant)
-	app.Handle(http.MethodDelete, "/roles/{id}/access/{access_id}", roles.Revoke)
+	{
+		roles := controllers.Roles{Db: db, Log: log}
+		app.Handle(http.MethodGet, "/roles", roles.List)
+		app.Handle(http.MethodGet, "/roles/{id}", roles.View)
+		app.Handle(http.MethodPost, "/roles", roles.Create)
+		app.Handle(http.MethodPut, "/roles/{id}", roles.Update)
+		app.Handle(http.MethodDelete, "/roles/{id}", roles.Delete)
+		app.Handle(http.MethodPost, "/roles/{id}/access/{access_id}", roles.Grant)
+		app.Handle(http.MethodDelete, "/roles/{id}/access/{access_id}", roles.Revoke)
+	}
 
 	// Access Routing
-	access := controllers.Access{Db: db, Log: log}
-	app.Handle(http.MethodGet, "/access", access.List)
+	{
+		access := controllers.Access{Db: db, Log: log}
+		app.Handle(http.MethodGet, "/access", access.List)
+	}
 
 	return app
 }
