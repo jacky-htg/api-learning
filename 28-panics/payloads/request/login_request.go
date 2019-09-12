53d8dbd31df050657c654ec4eff50e9dcb4390a0
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-09-12 22:28:30 +0700

    delete 28-panics

--- 28-panics/payloads/request/login_request.go
+++
@@ -1,7 +0,0 @@
-package request
-
-//LoginRequest : format json request for login
-type LoginRequest struct {
-	Username string `json:"username"  validate:"required"`
-	Password string `json:"password"  validate:"required"`
-}
--- 28-panics/payloads/response/token_response.go
+++
@@ -1,6 +0,0 @@
-package response
-
-//TokenResponse : format json response for token
-type TokenResponse struct {
-	Token string `json:"token"`
-}
