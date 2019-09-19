68f340594826b533e2497c0a0d776da22cd3a3b3
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-09-19 15:54:57 +0700

    rename services directory to libraries

--- 10-payloads/main.go
+++ 10-payloads/main.go
@@ -11,9 +11,9 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jacky-htg/go-services/libraries/config"
+	"github.com/jacky-htg/go-services/libraries/database"
 	"github.com/jacky-htg/go-services/routing"
-	"github.com/jacky-htg/go-services/services/config"
-	"github.com/jacky-htg/go-services/services/database"
 )
 
 func main() {
