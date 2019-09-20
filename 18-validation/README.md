0a5a0b2366bf8eecc6e69635fef4963b7545e908
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-09-20 21:21:34 +0700

    validation

---
+++ 18-validation/README.md
@@ -0,0 +1,18 @@
+# Rebel Services
+
+Validation
+
+Create validation on every request payloads.
+
+Task
+- add validation on decode function (libraries/api/request.go)
+- add validation tag on every struct of request payloads 
+
+## File Changes :
+- libraries/api/request.go
+- packages/auth/payloads/request/user_request.go
+
+## New File :
+
+## Adding Dependency :
+- gopkg.in/go-playground/validator.v9
---
+++ 18-validation/packages/auth/payloads/request/user_request.go
@@ -0,0 +1,55 @@
+package request
+
+import (
+	"github.com/jacky-htg/go-services/packages/auth/models"
+)
+
+//NewUserRequest : format json request for new user
+type NewUserRequest struct {
+	Username   string `json:"username" validate:"required"`
+	Email      string `json:"email" validate:"required"`
+	Password   string `json:"password" validate:"required"`
+	RePassword string `json:"re_password" validate:"required"`
+	IsActive   bool   `json:"is_active"`
+}
+
+//Transform NewUserRequest to User
+func (u *NewUserRequest) Transform() *models.User {
+	var user models.User
+	user.Username = u.Username
+	user.Email = u.Email
+	user.Password = u.Password
+	user.IsActive = u.IsActive
+
+	return &user
+}
+
+//UserRequest : format json request for user
+type UserRequest struct {
+	ID         uint64 `json:"id,omitempty" validate:"required"`
+	Username   string `json:"username,omitempty" validate:"required"`
+	Email      string `json:"email,omitempty" validate:"required"`
+	Password   string `json:"password,omitempty" validate:"required"`
+	RePassword string `json:"re_password,omitempty" validate:"required"`
+	IsActive   bool   `json:"is_active,omitempty"`
+}
+
+//Transform NewUserRequest to User
+func (u *UserRequest) Transform(user *models.User) *models.User {
+	if u.ID == user.ID {
+		if len(u.Username) > 0 {
+			user.Username = u.Username
+		}
+
+		if len(u.Email) > 0 {
+			user.Email = u.Email
+		}
+
+		if len(u.Password) > 0 {
+			user.Password = u.Password
+		}
+
+		user.IsActive = u.IsActive
+	}
+	return user
+}
