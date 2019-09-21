bb1336a1074cee099e2f3f14d5b0af0f1c2521f3
Jacky Hutagalung <rijal.asep.nugroho@gmail.com>
2019-09-21 19:11:27 +0700

    design rbac routing

--- 21-authorization-using-rbac/routing/route.go
+++ 21-authorization-using-rbac/routing/route.go
@@ -37,5 +37,23 @@ func API(db *sql.DB, log *log.Logger) http.Handler {
 		app.Handle(http.MethodDelete, "/users/:id", user.Delete)
 	}
 
+	// Roles Routing
+	{
+		roles := controllers.Roles{Db: db, Log: log}
+		app.Handle(http.MethodGet, "/roles", roles.List)
+		app.Handle(http.MethodGet, "/roles/:id", roles.View)
+		app.Handle(http.MethodPost, "/roles", roles.Create)
+		app.Handle(http.MethodPut, "/roles/:id", roles.Update)
+		app.Handle(http.MethodDelete, "/roles/:id", roles.Delete)
+		app.Handle(http.MethodPost, "/roles/:id/access/:access_id", roles.Grant)
+		app.Handle(http.MethodDelete, "/roles/:id/access/:access_id", roles.Revoke)
+	}
+
+	// Access Routing
+	{
+		access := controllers.Access{Db: db, Log: log}
+		app.Handle(http.MethodGet, "/access", access.List)
+	}
+
 	return app
 }
